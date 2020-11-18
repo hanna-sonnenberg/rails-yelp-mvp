9276b116703523a56363f9ca809b0115053942cc
hanna sonnenberg <sonnenberghanna@googlemail.com>
2020-11-18 14:54:44 +0100

    added validations for restaurant and review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :rating, presence: true
+  validates :rating, :inclusion => 0..5, numericality: {only_integer: true}
+  validates :content, presence: true
 end
