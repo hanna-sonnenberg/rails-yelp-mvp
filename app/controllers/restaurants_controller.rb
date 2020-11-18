bbd3eaad73f393d354eb04dfd41e06ef069163ce
hanna sonnenberg <sonnenberghanna@googlemail.com>
2020-11-18 20:15:44 +0100

    added create action in restaurants-controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -26,6 +26,6 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :rating)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
