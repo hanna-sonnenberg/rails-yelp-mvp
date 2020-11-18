30109724b4091fc2d52226d66812262a7c76dba3
hanna sonnenberg <sonnenberghanna@googlemail.com>
2020-11-18 20:57:38 +0100

    finish review controller and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,26 @@
 class ReviewsController < ApplicationController
   # TO-DO: GET "restaurants/38/reviews/new"
   # TO-DO: POST "restaurants/38/reviews"
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
